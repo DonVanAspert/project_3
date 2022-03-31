553f2c71539b26e78529ef80ff34bd39da717722
Kappugino <tygo.bovens@edu-kw1c.nl>
2022-03-31 14:30:39 +0200

    oi bruv want some tea

---
+++ pagina's/vragen.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+    Auteur: <tygo bovens>
+    Aanmaakdatum: <20/10/2021>
+
+    template
+-->
+<html>
+<head>
+    <title>
+        oi bruv
+    </title>
+    <meta charset="utf_8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="" rel="">
+</head>
+<body>
+
+</body>
+</html>
