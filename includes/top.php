d7cb12bd9e88aad01ece5f50ec2febf49ac32b09
Kappugino <tygo.bovens@edu-kw1c.nl>
2022-04-05 10:06:22 +0200

    includes

---
+++ includes/top.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+    Auteur: <tygo bovens>
+    Aanmaakdatum: <20/10/2021>
+
+    template
+-->
+<html>
+<head>
+    <title>
+        oi bruv
+    </title>
+    <meta charset="utf_8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../styles/stylesheet.css" rel="stylesheet">
+</head>
+<body>
+<p>oi</p>
+</body>
+</html>
