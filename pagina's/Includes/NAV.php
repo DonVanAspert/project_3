e6602f65bee50adada3f1926b265aefaf5c892f7
milklover4000 <arda.agar@edu-kw1c.nl>
2022-04-05 10:04:52 +0200

    Commit NAV

---
+++ pagina's/Includes/NAV.php
@@ -0,0 +1,24 @@
+<?php
+/**************************
+ * Bestand  : NAV.php
+ * User     : Arda Agar
+ * Datum    : 31-3-2022
+ ****************************/
+?>
+
+<nav>
+    <ul>
+        <li>
+            <a href="#" title="Aanbod van opleidingen"> Opleidingen</a>
+        </li>
+        <li>
+            <a href="#" title="Hulp bij studiekeuze">Studiekeuze</a>
+        </li>
+        <li>
+            <a href="#" title="Volwassenen en werkgevers">Volwassenen &amp; Werkgevers</a>
+        </li>
+        <li>
+            <a href="#" title="Informatie voor studenten">Studenteninfo</a>
+        </li>
+    </ul>
+</nav>
