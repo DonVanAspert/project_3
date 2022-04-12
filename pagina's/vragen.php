cc3d1b7f2ac87a9db44a42858fbae28d99fcedf2
milklover4000 <arda.agar@edu-kw1c.nl>
2022-04-12 11:03:10 +0200

    Commit vragenlijst

--- pagina's/vragen.php
+++ pagina's/vragen.php
@@ -49,61 +49,61 @@ include "../includes/NAV.php"
         Vraag 2: Heb je interresse van het coderen van software (bijv. games , apps of websites)
     </h3>
     <br>
-    <input type="radio" name="antwoord1"> Ja
+    <input type="radio" name="antwoord5"> Ja
     <br>
-    <input type="radio" name="antwoord2"> matig
+    <input type="radio" name="antwoord6"> matig
     <br>
-    <input type="radio" name="antwoord3"> nee
+    <input type="radio" name="antwoord7"> nee
     <br>
-    <input type="radio" name="antwoord4"> helemaal niet
+    <input type="radio" name="antwoord8"> helemaal niet
     <br>
     <h3>
     Vraag 3: Voel je je thuis in virtual reality? Communiceer je goed in bits & bytes? Vind je het ook leuk om dienstverlenend te zijn?
     </h3>
     <br>
-    <input type="radio" name="antwoord1"> Ja
+    <input type="radio" name="antwoord9"> Ja
     <br>
-    <input type="radio" name="antwoord2"> matig
+    <input type="radio" name="antwoord10"> matig
     <br>
-    <input type="radio" name="antwoord3"> nee
+    <input type="radio" name="antwoord11"> nee
     <br>
-    <input type="radio" name="antwoord4"> helemaal niet
+    <input type="radio" name="antwoord12"> helemaal niet
     <br>
     <h3>
     Vraag 4: Wil jij later verantwoordelijk zijn voor de informatiesystemen waar de moderne maatschappij op draait?
     </h3>
     <br>
-    <input type="radio" name="antwoord1"> Ja
+    <input type="radio" name="antwoord13"> Ja
     <br>
-    <input type="radio" name="antwoord2"> matig
+    <input type="radio" name="antwoord14"> matig
     <br>
-    <input type="radio" name="antwoord3"> nee
+    <input type="radio" name="antwoord15"> nee
     <br>
-    <input type="radio" name="antwoord4"> helemaal niet
+    <input type="radio" name="antwoord16"> helemaal niet
     <br>
     <h3>
         Vraag 5: Vindt je het leuk om computers te bouwen
     </h3>
     <br>
-    <input type="radio" name="antwoord1"> Ja, het lijkt me leuk
+    <input type="radio" name="antwoord17"> Ja, het lijkt me leuk
     <br>
-    <input type="radio" name="antwoord2"> Ik heb al een gebouwt
+    <input type="radio" name="antwoord18"> Ik heb al een gebouwt
     <br>
-    <input type="radio" name="antwoord3"> nee
+    <input type="radio" name="antwoord19"> nee
     <br>
-    <input type="radio" name="antwoord4"> Ik heb er niet een gebouwt maar lijkt me wel leuk
+    <input type="radio" name="antwoord20"> Ik heb er niet een gebouwt maar lijkt me wel leuk
     <br>
     <h3>
     Vraag 6: heb je interesse gehad om websites of software te programeren
     </h3>
     <br>
-    <input type="radio" name="antwoord1"> Ja, het lijkt me leuk
+    <input type="radio" name="antwoord21"> Ja, het lijkt me leuk
     <br>
-    <input type="radio" name="antwoord2"> Ik heb al een website gemaakt
+    <input type="radio" name="antwoord22"> Ik heb al een website gemaakt
     <br>
-    <input type="radio" name="antwoord3"> nee
+    <input type="radio" name="antwoord23"> nee
     <br>
-    <input type="radio" name="antwoord4"> Ik heb geen website gemaakt maar het lijkt wel leuk
+    <input type="radio" name="antwoord244"> Ik heb geen website gemaakt maar het lijkt wel leuk
     <br>
 </form>
 </body>
