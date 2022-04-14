8d36e0c8c846fb989c1885cb9c13485961ab7b3a
milklover4000 <arda.agar@edu-kw1c.nl>
2022-04-14 14:09:39 +0200

    Commit vragenlijst

--- pagina's/vragen.php
+++ pagina's/vragen.php
@@ -31,7 +31,12 @@ include "../includes/NAV.php"
 </h1>
 <form action="uitslag.php" method="post">
     <?php
-    //
+    // <input type="radio" id="html" name="fav_language" value="HTML">
+    //  <label for="html">HTML</label><br>
+    //  <input type="radio" id="css" name="fav_language" value="CSS">
+    //  <label for="css">CSS</label><br>
+    //  <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+    //  <label for="javascript">JavaScript</label>
     ?>
     <h3>
         Vraag 1: Heb jij echt verstand van computers en netwerken? Vind je het ook leuk om dienstverlenend te zijn?
@@ -106,8 +111,87 @@ include "../includes/NAV.php"
     <input type="radio" name="antwoord24"> Ik heb geen website gemaakt maar het lijkt wel leuk
     <br>
     <h3>
-        vraag 7:
+        vraag 7: vind je het leuk om met virtuele machines te werken
     </h3>
+    <input type="radio" name="antwoord25"> Ja, het lijkt me leuk
+    <br>
+    <input type="radio" name="antwoord26"> Ik heb al eens met een virtuele machine gewerkt
+    <br>
+    <input type="radio" name="antwoord27"> nee
+    <br>
+    <input type="radio" name="antwoord28"> Ik heb geen verstand van virtuele machines maar het lijkt wel iets voor mij
+    <h3>
+        vraag 8: heb je ooit willen weten hoe ze video games maken?
+    </h3>
+    <input type="radio" name="antwoord29"> Ja
+    <br>
+    <input type="radio" name="antwoord30"> Ik heb van anderen gehoord/gezien hoe het gedaan wordt en heb zin
+    <br>
+    <input type="radio" name="antwoord31"> nee, ik heb geen zin
+    <h3>
+        vraag 9: heb je interresse om computers/servers op te zetten
+    </h3>
+      <input type="radio" id="Ja" name="vraag9" value="ja">
+      <label for="ja"></label>Ik heb van anderen gehoord/gezien hoe het gedaan wordt en heb zin<br>
+      <input type="radio" id="Nee" name="vraag9" value="nee">
+      <label for="nee"></label>nee, ik heb geen zin in het opzetten<br>
+      <input type="radio" id="Matig" name="vraag9" value="matig">
+      <label for="matig"> ik heb een klein verstand van het bouwen van computers.</label>
+
+    <h3>
+        vraag 10: kan je lang achter een computerscherm zitten?
+    </h3>
+      <input type="radio" id="Ja" name="vraag10" value="ja">
+      <label for="ja">Ja, dat kan ik</label><br>
+      <input type="radio" id="Nee" name="vraag10" value="nee">
+      <label for="nee">nee, ik ben actief</label><br>
+      <input type="radio" id="Matig" name="vraag10" value="matig">
+      <label for="matig">ik kan soms lang en soms niet lang zitten.</label>
+    <h3>
+        vraag 11: kan je lang achter een computerscherm zitten?
+    </h3>
+      <input type="radio" id="Ja" name="vraag11" value="ja">
+      <label for="ja">Ja, dat kan ik</label><br>
+      <input type="radio" id="Nee" name="vraag11" value="nee">
+      <label for="nee">nee, ik ben actief</label><br>
+      <input type="radio" id="Matig" name="vraag11" value="matig">
+      <label for="matig">ik kan soms lang en soms niet lang zitten.</label>
+    <h3>
+        vraag 12: kan je lang achter een computerscherm zitten?
+    </h3>
+      <input type="radio" id="Ja" name="vraag10" value="ja">
+      <label for="ja">Ja, dat kan ik</label><br>
+      <input type="radio" id="Nee" name="vraag10" value="nee">
+      <label for="nee">nee, ik ben actief</label><br>
+      <input type="radio" id="Matig" name="vraag10" value="matig">
+      <label for="matig">ik kan soms lang en soms niet lang zitten.</label>
+    <h3>
+        vraag 13: kan je lang achter een computerscherm zitten?
+    </h3>
+      <input type="radio" id="Ja" name="vraag10" value="ja">
+      <label for="ja">Ja, dat kan ik</label><br>
+      <input type="radio" id="Nee" name="vraag10" value="nee">
+      <label for="nee">nee, ik ben actief</label><br>
+      <input type="radio" id="Matig" name="vraag10" value="matig">
+      <label for="matig">ik kan soms lang en soms niet lang zitten.</label>
+    <h3>
+        vraag 14: kan je lang achter een computerscherm zitten?
+    </h3>
+      <input type="radio" id="Ja" name="vraag10" value="ja">
+      <label for="ja">Ja, dat kan ik</label><br>
+      <input type="radio" id="Nee" name="vraag10" value="nee">
+      <label for="nee">nee, ik ben actief</label><br>
+      <input type="radio" id="Matig" name="vraag10" value="matig">
+      <label for="matig">ik kan soms lang en soms niet lang zitten.</label>
+    <h3>
+        vraag 15: kan je lang achter een computerscherm zitten?
+    </h3>
+      <input type="radio" id="Ja" name="vraag10" value="ja">
+      <label for="ja">Ja, dat kan ik</label><br>
+      <input type="radio" id="Nee" name="vraag10" value="nee">
+      <label for="nee">nee, ik ben actief</label><br>
+      <input type="radio" id="Matig" name="vraag10" value="matig">
+      <label for="matig">ik kan soms lang en soms niet lang zitten.</label>
     <br>
     <br>
     <input type="submit" name="verzend">
